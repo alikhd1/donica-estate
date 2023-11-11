b3275c326641b5fe52742e40dd70937d38f3574b
Ali khandi <alikhandi@gmail.com>
2023-11-11 03:45:35 +0330

    fix dokcer error

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,6 @@ RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
 COPY ./app /code/app
 
+CMD ["python", "fake_data.py"]
+
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "3000"]
--- app/settings.py
+++ app/settings.py
@@ -7,7 +7,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "dornicagres")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres")
 POSTGRES_USER = os.getenv("POSTGRES_USER", "dornica")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "very_hard_to_guess")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "dornica")
